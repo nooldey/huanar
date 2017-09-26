4923bed6d900866bc581da950d960d8724750222
nooldey <openzws@163.com>
2017-09-26 18:34:34 +0800

    fix

--- src/extend/utils.ts
+++ src/extend/utils.ts
@@ -1,14 +1,10 @@
+import errmap from './error_code'
+
 const handleErr = (err: any) => {
     if (!err) {
         return 'error'
     }
     let e = JSON.parse(JSON.stringify(err))
-    let errmap = {
-        '1': '服务器内部错误或者参数错误',
-        '100': '无法建立 TCP 连接到 LeanCloud 服务器',
-        '101': '查询对象不存在',
-        '103': '非法的 Class 名称'
-    }
     let toast = (<any>errmap)[e.code] || '请求失败'
     alert(toast)
 }
