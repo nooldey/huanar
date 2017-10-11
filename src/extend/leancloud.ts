76e0ce9024cae9354791ea471eeb49f8abe10a92
nooldey <openzws@163.com>
2017-10-11 18:51:17 +0800

    renwn

--- src/extend/leancloud.ts
+++ src/extend/leancloud.ts
@@ -2,6 +2,7 @@
 import AV from 'leancloud-storage'
 
 /* Init Leancloud Storage */
+/* NOTE: 将以下的秘钥对修改为你的秘钥对，否则项目无法正常使用 */
 const appId = 'jFxubcUYxlt4z3XtDCXit7M7-gzGzoHsz'
 const appKey = 'yvxiyhmBz66Xoq0X6hyrww56'
 
